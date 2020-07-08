35ab8cff06552a14372c74c6226d2f4842eb4fcd
Fatah <derradjfatah@gmail.com>
2020-07-08 15:24:55 +0200

    Add a new user
    Update the controller

--- src/DataFixtures/CustomerFixtures.php
+++ src/DataFixtures/CustomerFixtures.php
@@ -15,24 +15,27 @@ class CustomerFixtures extends Fixture
             ->setLastname('DUPONT')
             ->setEmail('jeand@gmail.com');
 
+        $manager->persist($customer1);
+
         $customer2 = (new Customer())
             ->setFirstname('Christelle')
             ->setLastname('DURANT')
             ->setEmail('christelled@gmail.com');
+
+        $manager->persist($customer2);
 
         $customer3 = (new Customer())
             ->setFirstname('Alain')
             ->setLastname('DUPUIS')
             ->setEmail('alaind@gmail.com');
 
+        $manager->persist($customer3);
+
         $customer4 = (new Customer())
             ->setFirstname('Claire')
             ->setLastname('SMITH')
             ->setEmail('clairesmith@gmail.com');
 
-        $manager->persist($customer1);
-        $manager->persist($customer2);
-        $manager->persist($customer3);
         $manager->persist($customer4);
 
         $manager->flush();
